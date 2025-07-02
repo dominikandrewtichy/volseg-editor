10025295067eec94c3599a328ec12f1b6a29e7b4
Dominik Tichy <tichydominik451@gmail.com>
2025-07-03 00:22:13 +0200

    init migration

--- backend/app/database/models/role_model.py
+++ backend/app/database/models/role_model.py
@@ -1,5 +1,6 @@
 from enum import Enum
 
+from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.database.models.base_model import Base
@@ -14,7 +15,7 @@ class RoleEnum(str, Enum):
 class Role(Base, UuidMixin):
     __tablename__ = "roles"
 
-    name: Mapped[str] = mapped_column(unique=True)
+    name: Mapped[str] = mapped_column(String(255), nullable=False, unique=True, index=True)
     description: Mapped[str | None] = mapped_column(default=None)
 
     users: Mapped[list["User"]] = relationship(back_populates="role")
