56e46ff15d3c53eada799d363a84568de0f28245
Dominik Tichy <tichydominik451@gmail.com>
2025-07-02 23:47:40 +0200

    added alembic

--- backend/app/database/models/__init__.py
+++ backend/app/database/models/__init__.py
@@ -1,4 +1,7 @@
+from .base_model import Base
 from .entry_model import Entry
+from .role_model import Role
 from .share_link_model import ShareLink
 from .user_model import User
 from .view_model import View
+from .volseg_entry_model import VolsegEntry
