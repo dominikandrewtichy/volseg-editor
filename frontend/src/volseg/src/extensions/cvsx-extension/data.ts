82e9c0a3c8abd9677f4252a2f0de21860666199b
Dominik Tichy <tichydominik451@gmail.com>
2025-07-02 21:24:40 +0200

    ignore build for all volseg files

--- frontend/src/volseg/src/extensions/cvsx-extension/data.ts
+++ frontend/src/volseg/src/extensions/cvsx-extension/data.ts
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
 /**
  * Copyright (c) 2018-2024 mol* contributors, licensed under MIT, See LICENSE file for more info.
  *
