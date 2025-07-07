136300ab24fb44973998c03c16f7c95d1d0518dc
Dominik Tichy <tichydominik451@gmail.com>
2025-07-07 02:07:06 +0200

    wip

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -17,7 +17,6 @@ client.setConfig({
 
 async function myInterceptor(response: Response) {
   if (response.status === 401) {
-    console.log("REDIRECTINGS");
     window.location.href = "/login";
   }
   return response;
