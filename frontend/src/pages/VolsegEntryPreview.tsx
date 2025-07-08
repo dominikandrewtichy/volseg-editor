60f67ec1dd159e8ef38967a5f4e85e5b3193a787
Dominik Tichy <tichydominik451@gmail.com>
2025-07-08 20:14:27 +0200

    loading skeletons

--- frontend/src/pages/VolsegEntryPreview.tsx
+++ frontend/src/pages/VolsegEntryPreview.tsx
@@ -1,11 +1,12 @@
 import { VisibilityBadge } from "@/components/common/VisibilityBadge";
+import { Skeleton } from "@/components/ui/skeleton";
 import { useMolstar } from "@/contexts/MolstarProvider";
 import { useRequiredParam } from "@/hooks/useRequiredParam";
 import { volsegEntriesGetEntryByIdOptions } from "@/lib/client/@tanstack/react-query.gen";
 import { formatDate } from "@/lib/utils";
 import { useQuery } from "@tanstack/react-query";
 import { Calendar } from "lucide-react";
-import { lazy, useEffect } from "react";
+import { lazy, Suspense, useEffect } from "react";
 
 const MolstarViewer = lazy(() => import("../components/molstar/MolstarViewer"));
 
@@ -29,35 +30,41 @@ export function VolsegEntryPreview() {
     loadVolseg();
   }, [entryId, viewer]);
 
-  if (volsegEntryQuery.isLoading) {
-    return (
-      <div className="container py-8">
-        <div className="text-center py-8">Loading entry data...</div>
-      </div>
-    );
-  }
-
   return (
-    <>
-      {volsegEntryQuery.data && (
-        <div className="container py-8">
-          <div className="md:col-span-2">
-            <div className="flex justify-between items-start mb-4">
-              <h1 className="text-3xl font-bold">
-                {volsegEntryQuery.data.name}
-              </h1>
-              <VisibilityBadge isPublic={volsegEntryQuery.data.is_public} />
-            </div>
-            <div className="flex items-center text-sm text-muted-foreground mb-6">
-              <Calendar className="h-4 w-4 mr-2" />
-              Created on {formatDate(volsegEntryQuery.data?.created_at)}
-            </div>
-          </div>
-          <div className="flex-1 relative h-[800px]">
-            <MolstarViewer />
-          </div>
+    <div className="container py-8">
+      <div className="md:col-span-2">
+        <div className="flex justify-between items-start mb-4">
+          {volsegEntryQuery.data ? (
+            <h1 className="text-3xl font-bold">
+              {volsegEntryQuery.data.name ?? ""}
+            </h1>
+          ) : (
+            <Skeleton className="h-9 w-64" />
+          )}
+
+          {volsegEntryQuery.data ? (
+            <VisibilityBadge
+              isPublic={volsegEntryQuery.data.is_public ?? false}
+            />
+          ) : (
+            <Skeleton className="h-5 w-14" />
+          )}
         </div>
-      )}
-    </>
+
+        {volsegEntryQuery.data ? (
+          <div className="flex flex-row items-center text-sm text-muted-foreground">
+            <Calendar className="h-4 w-4 mr-2" />
+            Created on {formatDate(volsegEntryQuery.data?.created_at ?? "")}
+          </div>
+        ) : (
+          <Skeleton className="h-5 w-64" />
+        )}
+      </div>
+      <div className="flex-1 relative h-[800px] mt-6">
+        <Suspense fallback={<Skeleton className="size-full" />}>
+          <MolstarViewer />
+        </Suspense>
+      </div>
+    </div>
   );
 }
