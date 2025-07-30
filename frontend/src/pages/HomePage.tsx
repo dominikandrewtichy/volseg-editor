214665ed09484145f92c5c93fd97066c7726c934
Dominik Tichy <tichydominik451@gmail.com>
2025-07-30 17:59:05 +0200

    title

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,48 +1,19 @@
-import { PaginationControls } from "@/components/common/Pagination";
-import { SearchBar } from "@/components/common/SearchBar";
 import { EntryPreview } from "@/components/entries/EntryPreview";
 import { Button } from "@/components/ui/button";
+import { EntryResponse, PaginatedResponseEntryResponse } from "@/lib/client";
 import { entriesListPublicEntriesOptions } from "@/lib/client/@tanstack/react-query.gen";
 import { useQuery } from "@tanstack/react-query";
-import { useNavigate, useSearchParams } from "react-router";
+import { useNavigate } from "react-router";
 
 export function HomePage() {
-  const [searchParams, setSearchParams] = useSearchParams();
-
   const navigate = useNavigate();
-  const search_term = searchParams.get("search_term") || "";
-  // const page = parseInt(searchParams.get("page") || "1");
-  const perPage = parseInt(searchParams.get("per_page") || "12");
-
-  const { data, isLoading, error } = useQuery({
+  const { data, error, isLoading, isSuccess, isError } = useQuery({
     ...entriesListPublicEntriesOptions({
       query: {},
     }),
   });
 
-  const handleSearch = (searchTerm: string) => {
-    setSearchParams({
-      search_term: searchTerm,
-      page: "1",
-      per_page: perPage.toString(),
-    });
-  };
-
-  const handlePageChange = (newPage: number) => {
-    setSearchParams({
-      search_term: search_term,
-      page: newPage.toString(),
-      per_page: perPage.toString(),
-    });
-  };
-
-  const handlePerPageChange = (newPerPage: number) => {
-    setSearchParams({
-      search_term: search_term,
-      page: "1",
-      per_page: newPerPage.toString(),
-    });
-  };
+  const entries = data as PaginatedResponseEntryResponse;
 
   return (
     <div className="space-y-24">
@@ -56,7 +27,7 @@ export function HomePage() {
 
       <section>
         <div className="flex justify-between items-center mb-6">
-          <h2 className="text-2xl font-semibold">Featured entries</h2>
+          <h2 className="text-2xl font-semibold">Public entries</h2>
           <Button variant="default" onClick={() => navigate("/entries/new")}>
             Create Entry
           </Button>
@@ -65,15 +36,15 @@ export function HomePage() {
         {isLoading && (
           <div className="text-center py-12">Loading entries...</div>
         )}
-        {error && (
+        {isError && (
           <div className="text-center py-12 text-red-500">
             Error loading entries. Please try again.
           </div>
         )}
-        {data?.items.length !== 0 && (
+        {isSuccess && (
           <>
             <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mb-8">
-              {data?.items.map((entry) => (
+              {entries.items.map((entry) => (
                 <EntryPreview key={entry.id} entry={entry} />
               ))}
             </div>
